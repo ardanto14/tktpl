dde9bccedd50962a6e69b1b0bb2f06f03572cf8d
ardanto14 <ardantofinsep14@gmail.com>
2020-09-18 00:27:57 +0700

    add simple functionality

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
+
+    TextView txtCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.txtCount = (TextView) findViewById(R.id.textView3);
+    }
+
+    public void increment(View view) {
+        this.count++;
+        this.txtCount.setText(String.valueOf(count));
+    }
+
+    public void decrement(View view) {
+        this.count--;
+        this.txtCount.setText(String.valueOf(count));
     }
 }
